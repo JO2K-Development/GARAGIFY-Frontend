80d59e3b5aa3d94352dbee88f3499afe5a3f4164
Karol poziomk3 Zając <karolzajac.0407@gmail.com>
2025-06-01 10:57:20 +0200

    feat: refactor

--- src/app/borrow/page.tsx
+++ src/app/borrow/page.tsx
@@ -1,15 +1,6 @@
 "use client";
-import { getLendTimeRanges } from "@/api/availability";
 import BorrowingView from "@/components/Borrowing/BorrowingView/BorrowingView";
-import { useEffect } from "react";
 
 export default function Page() {
-  useEffect(() => {
-    getLendTimeRanges(1,
-      {
-        untilWhen: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7 days from now
-      }
-    )
-  }, []);
   return <BorrowingView />;
 }
