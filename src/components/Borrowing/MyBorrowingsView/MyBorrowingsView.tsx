d08c78cf82d5ddcf204bf61f797fce15ad010be8
therockey <kacperus.tomczyk@gmail.com>
2025-06-05 00:05:00 +0200

    Remove unused code

--- src/components/Borrowing/MyBorrowingsView/MyBorrowingsView.tsx
+++ src/components/Borrowing/MyBorrowingsView/MyBorrowingsView.tsx
@@ -3,21 +3,6 @@ import styles from "./MyBorrowingsView.module.scss";
 import useMyBorrowingsView from "@/components/Borrowing/MyBorrowingsView/useMyBorrowingsView";
 import LendBorrowWidget from "@/components/LendBorrowWidget/LendBorrowWidget";
 
-const borrowings = [
-  {
-    id: "1",
-    start_date: "2025-06-01T05:20:34.551Z",
-    end_date: "2025-06-04T14:46:34.551Z",
-    owner: { email: "user1@example.com" }
-  },
-  {
-    id: "2",
-    start_date: "2025-06-04T14:46:34.551Z",
-    end_date: "2025-06-04T14:46:34.551Z",
-    owner: { email: "user2@example.com" }
-  }
-];
-
 const MyBorrowingsView = () => {
   const {
     content,
--- src/components/Lending/MyLendingsView/MyLendingsView.tsx
+++ src/components/Lending/MyLendingsView/MyLendingsView.tsx
@@ -3,24 +3,6 @@ import styles from "./MyLendingsView.module.scss";
 import LendBorrowWidget from "@/components/LendBorrowWidget/LendBorrowWidget";
 import useMyLendingsView from "./useMyLendingsView";
 
-const lendings = [
-  {
-    id: "1",
-    start_date: "2025-06-01T05:20:34.551Z",
-    end_date: "2025-06-04T14:46:34.551Z",
-    borrowers: [
-      { email: "user1@example.com", user_id: "user1" },
-      { email: "user2@example.com", user_id: "user2" }
-    ],
-  },
-  {
-    id: "2",
-    start_date: "2025-06-04T14:46:34.551Z",
-    end_date: "2025-06-04T14:46:34.551Z",
-    borrowers: []
-  }
-];
-
 const MyLendingsView = () => {
   const {
     content,
